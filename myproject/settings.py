61f0ab5d5d3b52eb8b530b79b2679447c2b6dfb1
hiralpatil116-ui <aapka_email@example.com>
2026-09-10 01:28:15 +0530

    Fix image paths on live database

--- myproject/settings.py
+++ myproject/settings.py
@@ -109,10 +109,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
